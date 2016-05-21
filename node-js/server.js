8671add565e5a45209f8f977c9c1661d12f1da5e
chicobentojr <francisco.bento.jr@hotmail.com>
2016-05-21 20:44:17 -0300

    Add command to automatically open browser

--- node-js/server.js
+++ node-js/server.js
@@ -1,4 +1,5 @@
 const app = require('./config/express')();
+const open = require('open');
 
 app.get('/', function(request, response){
   response.send('<h1>Hello World! </h1>');
@@ -6,4 +7,6 @@ app.get('/', function(request, response){
 
 app.listen('3000', function(){
   console.log('server running..');
+  console.log('browser opening..');
+  open('http://localhost:3000/');
 })
